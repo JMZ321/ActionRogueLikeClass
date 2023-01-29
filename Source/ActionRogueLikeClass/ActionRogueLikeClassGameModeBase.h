871dbca4dee6721141b8a170193a3c2a3898adf3
JMZ321 <58113557+JMZ321@users.noreply.github.com>
2023-01-28 19:14:34 -0500

    Creating UE Project
    - Added Class assets
    - Added input mapping Forward and turn

---
+++ Source/ActionRogueLikeClass/ActionRogueLikeClassGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ActionRogueLikeClassGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONROGUELIKECLASS_API AActionRogueLikeClassGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
